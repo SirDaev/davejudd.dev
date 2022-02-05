cef4af74f22c59608103ffb18af2ff1bd9536f95
Dave Judd <52185dj@gmail.com>
2022-02-05 11:27:39 -0800

    Apply basic layout and style

---
+++ src/components/header.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<header>
+  Header
+</header>
+
+<style>
+  header {
+    background-color: #ddd;
+  }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<link rel="stylesheet" href="/reset.css">
+</svelte:head>
+
+<script>
+  import Header from '../components/header.svelte';
+</script>
+
+<Header />
+<main>
+  <slot></slot>
+</main>
+<footer>footer</footer>
